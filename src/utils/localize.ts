e93aef351ec4eab4c59f1d9b56e88667bbb6d013
saifhany <saifhany898@gmail.com>
2021-05-17 12:41:29 +0200

    y

--- src/utils/localize.ts
+++ src/utils/localize.ts
@@ -1,6 +1,6 @@
 import { lang } from "./enums";
-const ar = require("../localize/ar.json");
-const en = require("../localize/en.json");
+import * as  ar  from '../localize/ar.json';
+import * as en from '../localize/en.json';
 
 export class Localize {
   private static language: lang = lang.Arabic;
@@ -13,6 +13,7 @@ export class Localize {
       case lang.English:
         return en[text];
         break;
+
     }
   };
   public static localize = (lang: lang, text) => {
